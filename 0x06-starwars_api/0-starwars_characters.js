4fdde656e1a06cfc45ea018b81c6b79c89810ded
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-06-13 13:18:02 +0300

    Refactor Star Wars character fetching script for improved error handling and clarity

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request');
+/*const request = require('request');
 const movieId = process.argv[2];
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
@@ -20,6 +20,40 @@ function printCharacters (characters, index) {
       if (index + 1 < characters.length) {
         printCharacters(characters, index + 1);
       }
+    }
+  });
+}*/
+
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(filmUrl, (err, res, body) => {
+  if (err || res.statusCode !== 200) {
+    console.error('Error fetching film:', err || `Status ${res.statusCode}`);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+  fetchCharacters(characters, 0);
+});
+
+function fetchCharacters(characters, index) {
+  if (index >= characters.length) return;
+
+  request(characters[index], (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+      fetchCharacters(characters, index + 1);
+    } else {
+      console.error('Error fetching character');
     }
   });
 }
